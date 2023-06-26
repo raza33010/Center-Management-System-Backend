9791e51482b40c6740b8e28d2803a6414a8c1a3f
Abbas Raza <raza33010@gmail.com>
2023-06-26 22:11:13 +0400

    first commit

--- c_user.py
+++ c_user.py
@@ -91,55 +91,6 @@ def get_all_users():
     response = {'code': '200', 'status': 'true', 'data': users}
     return jsonify(response)
 
-@app.route('/del_user/<int:id>', methods=['DELETE'])
-def delete_user(id):
-    cur = mysql.connection.cursor()
-    user = cur.execute(f"DELETE FROM c_user WHERE id={id}")
-    mysql.connection.commit()
-    cur.close()
-    if user > 0:
-        final_response = {'code': '200', 'status': 'true', 'message': 'user found', 'data': user}
-        return jsonify(final_response)
-    else:
-        final_response = {'code': '400', 'status': 'false', 'message': 'user not found', 'data': user}
-        return jsonify(final_response)
-
-
-@app.route('/upd_user/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    form = UserForm(request.form)
-    if form.validate():
-        center_id = form.center_id.data
-        name = form.name.data
-        role = form.role.data
-        status = form.status.data
-        updated_at = form.updated_at.data
-        
-        cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM c_user WHERE id=%s", (user_id,))
-        user = cur.fetchone()
-
-        if not user:
-            cur.close()
-            final_response = {'code': '404', 'status': 'false', 'message': 'user not found'}
-            return jsonify(final_response)
-        else:
-            cur.execute(f"SELECT * FROM center WHERE id = %s", (center_id,))
-            result = cur.fetchone()  # Fetch a single row
-
-            if result:
-                cur.execute("UPDATE c_user SET center_id=%s, name=%s, role=%s, status=%s, updated_at=%s WHERE id=%s", (center_id, name, role, status, updated_at, user_id))
-                mysql.connection.commit()
-                cur.close()
-                response = {'code': '200', 'status': 'true', 'message': 'user updated successfully'}
-                return jsonify(response)
-            else:
-                response = {'code': '400', 'status': 'false', 'message': 'user not updated successfully'}
-                return jsonify(response)
-    else:
-        final_response = {'code': '400', 'status': 'false', 'message': 'Invalid input'}
-        return jsonify(final_response)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
