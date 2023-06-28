781266a1901bf8ede307e36a753f34fcca3a5f76
Abbas Raza <raza33010@gmail.com>
2023-06-28 21:31:12 +0400

    first commit

--- Code/login.py
+++ Code/login.py
@@ -21,23 +21,6 @@ app.config['MYSQL_DB'] = 'crud'
 mysql = MySQL(app)
 
 
-@app.route('/insert', methods=['POST'])
-def insert():
-    form = StudentForm(request.form)
-    if form.validate():
-        name = form.name.data
-        email = form.email.data
-        phone = form.phone.data
-        cur = mysql.connection.cursor()
-        query = "INSERT INTO students (name, email, phone) VALUES (%s, %s, %s)"
-        fields = (name, email, phone)
-        cur.execute(query, fields)
-        mysql.connection.commit()
-        final_response = {'code': '200', 'status': 'true', 'message': 'inserted successfully'}
-        return jsonify(final_response)
-    else:
-        final_response = {'code': '400', 'status': 'false', 'message': 'data not inserted successfully'}
-        return jsonify(final_response)
 
 
 @app.route('/student', methods=['GET'])
@@ -48,50 +31,6 @@ def student():
     cur.close()
     return jsonify(data)
 
-
-@app.route('/student/<int:id>', methods=['GET'])
-def student_id(id):
-    cur = mysql.connection.cursor()
-    cur.execute(f"SELECT * FROM students WHERE id = {id}")
-    data = cur.fetchone()
-    cur.close()
-    if data:
-        return jsonify(data)
-    else:
-        final_response = {'code': '400', 'status': 'false', 'message': 'data not found'}
-        return jsonify(final_response)
-
-
-@app.route('/delete/<int:id_data>', methods=['DELETE'])
-def delete(id_data):
-    cur = mysql.connection.cursor()
-    data = cur.execute(f"DELETE FROM students WHERE id={id_data}")
-    mysql.connection.commit()
-    cur.close()
-    if data > 0:
-        final_response = {'code': '200', 'status': 'true', 'message': 'data found', 'data': data}
-        return jsonify(final_response)
-    else:
-        final_response = {'code': '400', 'status': 'false', 'message': 'data not found', 'data': data}
-        return jsonify(final_response)
-
-
-@app.route('/update', methods=['PUT'])
-def update():
-    form = StudentForm(request.form)
-    if form.validate():
-        id = request.form['id']
-        name = form.name.data
-        email = form.email.data
-        phone = form.phone.data
-        cur = mysql.connection.cursor()
-        cur.execute("""UPDATE students SET name=%s, email=%s, phone=%s WHERE id=%s""", (name, email, phone, id))
-        cur.close()
-        final_response = {'code': '200', 'status': 'True', 'message': 'data changed'}
-        return jsonify(final_response)
-    else:
-        final_response = {'code': '400', 'status': 'false', 'message': 'data unchanged'}
-        return jsonify(final_response)
 
 
 if __name__ == "__main__":
