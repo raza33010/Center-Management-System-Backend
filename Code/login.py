db1ec10c7498e117b10411d4c410c5a446e7dfa4
Abbas Raza <raza33010@gmail.com>
2023-06-29 22:13:17 +0400

    first commit

--- Code/login.py
+++ Code/login.py
@@ -5,7 +5,7 @@ from wtforms import Form, StringField, IntegerField, validators
 
 
 class StudentForm(Form):
-    name = StringField('Name', [validators.InputRequired()])
+    name = StringField('NAME', [validators.InputRequired()])
     email = StringField('Email', [validators.InputRequired(), validators.Email()])
     phone = StringField('Phone', [validators.InputRequired(), validators.Regexp('^\d{11}$', message='Phone number should be 11 digits')])
 
