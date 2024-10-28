9da6685ba3e8a186ac3d919ae4ed11c927cf3ee5
Irtiza1 <irtiza.s2918@gmail.com>
2024-10-28 11:56:21 +0500

    home page,result, academic,admin pages api done

--- Code/center.py
+++ Code/center.py
@@ -9,7 +9,8 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.secret_key = 'many random bytes'
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_PORT'] = 3306
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'center'
